f43ff8c57a8f256a2ab180e82906388bb24a47cd
vicksonzero <dicksonzero@gmail.com>
2014-12-04 12:15:49 +0000

    Imported PUN
    Made monster choosing menu v2

---
+++ Assets/Game/Multiplayer/MatchmakerBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MatchmakerBehaviour : MonoBehaviour {
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+	// Use this for initialization
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("0.1");
+        PhotonNetwork.logLevel = PhotonLogLevel.Full;
+        //PhotonNetworkingMessage
+
+	}
+	void OnGUI () {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+	}
+
+    void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    void OnPhotonRandomJoinFailed()
+    {
+        print("Can't join random room");
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    void OnJoinedRoom()
+    {
+        // spawn yourself
+        Application.LoadLevel("MultiplayerChoose");
+    }
+}
